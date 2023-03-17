baefc378e4008eb1a4a2590b9570b0805ddecdb3
SaerElMasri <saer1890@gmail.com>
2023-03-17 17:12:34 +0200

    Fixed styling

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,3 +1,10 @@
-document.querySelector('#login').onclick = () =>{
+const login_nav = document.querySelector('#login-nav');
+login_nav.addEventListener('click', () => {
     window.location.href = 'login.html';
-}
+    login_nav.classList.add("buttonAnimation");
+})
+const login_header = document.querySelector('#login-header');
+login_header.addEventListener('click', () => {
+    window.location.href = 'login.html';
+    login_header.classList.add("buttonAnimation");
+})
