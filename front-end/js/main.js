c36d2dc3203d7ef03e41f0aa46081b69a27ae928
SaerElMasri <saer1890@gmail.com>
2023-03-20 14:07:27 +0200

    Fixed styling of button on home

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,10 +1,10 @@
 const login_nav = document.querySelector('#login-nav');
 login_nav.addEventListener('click', () => {
     window.location.href = 'login.html';
-    login_nav.classList.add("buttonAnimation");
+    login_nav.classList.add("click-button");
 })
 const login_header = document.querySelector('#login-header');
 login_header.addEventListener('click', () => {
     window.location.href = 'login.html';
-    login_header.classList.add("buttonAnimation");
+    login_header.classList.add("click-button");
 })
